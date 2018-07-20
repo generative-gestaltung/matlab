afeae00cd828e586e31bfe84dc4bd4991383c94b
maxg <max.goettner@vw-fc.com>
2018-07-20 20:12:21 +0200

    next

--- newton.m
+++ newton.m
@@ -1,9 +1,9 @@
-function [y] = f1(x)
-
+function y = f1(x)
+  y = 3*x*x*x - 2*x*x + x + 3 
 end
 
-function [y] = dF1(x)
-
+function y = dF1(x)
+  y = 9*x*x - 4*x
 end
 
 function [ns] = newton (F, dF, x0, tol, n)
@@ -14,3 +14,8 @@ function [ns] = newton (F, dF, x0, tol, n)
     res = [res:xnext]
   end
 end
+
+
+
+
+
